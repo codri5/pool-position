3296609b94ea53c7853ed0244d7f5330a8359d2e
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-09 18:54:59 +0000

    added map and extracted coordinates and ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,36 @@
-// Included because my hamburger menu doesn't open, but as this is supposed to be a default in Bootstrap, maybe this is redundant?
+let map;
 
-$(document).ready(function(){
-    $(".navbar-toggler").click(function(){
-      $(".navbar-toggler").toggleClass("active");
-    });
-  });  
+// added and centered map
+function initMap() {
+  const wales = new google.maps.LatLng(52.1580, -3.9076);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: wales,
+    zoom: 8,
+  });
+}
 
-//   Slideshow to randomly display on fade-out-fade-in
+// spots array
+let spots = ['Dorothea Quarry', 'Llyn Tegid', 'Blue Lake', 'Pont Melin-Fach', 'Nant Gwrelych', 'Henrhyd Falls',
+'Pistyll Rhaeadr', 'Horseshoe Falls', 'Three Cliffs Bay', 'Rhossili Bay', 'Llynnau Mymbyr', 'Llyn Crafnant',
+'Llyn Dinas', 'River Towy', 'Llyn y Fan Fach'];
+// loop through spots array
+for (let i = 0; i < spots.length; i++) {
+  // call geocoding API and extract coordinates and place ids for each item
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${spots[i]}&key=AIzaSyB4_LlKcmjksW1RPD-Zm2y-t0JR7KMMtV4`)
+  .then((response) => {
+    return response.json();
+  }).then(jsonData => {
+    console.log(jsonData.results[0].place_id);
+    console.log(jsonData.results[0].geometry.location);
+   
+  })
+  .catch(error => {
+    console.log(error);
+  })
+}
+
+/*
+// Slideshow to randomly display on fade-out-fade-in
 let slideshow = document.querySelector("#slideshow");
 let slides = slideshow.querySelectorAll("img");
 let currentSlide = 0;
@@ -16,11 +40,4 @@ function nextSlide() {
   slides[currentSlide].classList.remove("active");
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].classList.add("active");
-}
-  function initMap() {
-    var wales = {lat: 52.1306, lng: -3.7837};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: wales
-    });
-  }
+}*/
