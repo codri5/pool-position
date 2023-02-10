e4d353ce89c6921e396bb13d2e509ae574218678
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-10 13:29:40 +0000

    added timeout function and for loop for markers

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,80 +7,26 @@ function initMap() {
     center: wales,
     zoom: 7,
   });
-    // added markers
-    new google.maps.Marker({
-      position: {lat: 52.8552183, lng: -3.3787533},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 53.0551749, lng: -4.2407414},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 51.88179659999999, lng: -3.7415411},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 52.8972141, lng: -3.6088066},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 51.79562439999999, lng: -3.664348099999999},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 52.981314, lng: -3.1997901},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 51.7822701, lng: -3.5845648},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 53.09786321255787, lng: -3.928736052823314},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 52.6887894, lng: -4.0417908},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 51.91971329382864, lng: -4.091270186035342},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 51.5780409, lng: -4.3001967},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 53.0684687, lng: -4.0768601},
-      map
-    });
-
-    new google.maps.Marker({
-      position: {lat: 53.0256944, lng: -4.0628135},
-      map
-    });
+  // added markers
+  setTimeout(function () {
+    for (let i = 0; i < spots.length; i++) {
+      new google.maps.Marker({
+        position: coord[i],
+        map
+      });
+    }
+  }, 100) 
 }
 
 // spots array
-const spots = ['Dorothea Quarry', 'Llyn Tegid', 'Blue Lake Wales', 'Pont Melin-Fach', 'Henrhyd Falls',
-              'Pistyll Rhaeadr', 'Horseshoe Falls Wales', 'Bae y Tri Chlogwyn', 'Rhossili Bay', 
-              'Llynnau Mymbyr', 'Llyn Dinas', 'River Towy', 'Llyn y Fan Fach'];
-// loop through spots array
+const spots = ['Dorothea Quarry', 'Llyn Tegid', 'Blue Lake Wales', 'Pont Melin-Fach', 
+              'Henrhyd Falls', 'Pistyll Rhaeadr', 'Horseshoe Falls Wales', 'Bae y Tri Chlogwyn', 
+              'Rhossili Bay', 'Llynnau Mymbyr', 'Llyn Dinas', 'River Towy', 'Llyn y Fan Fach'];
+
+// declare coord and id arrays
 const coord = [];
-const spotsID = [];
+const spotsID = []; 
+// loop through spots array
 for (let i = 0; i < spots.length; i++) {
   // call geocoding API and extract coordinates and place ids for each item
   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${spots[i]}&key=AIzaSyB4_LlKcmjksW1RPD-Zm2y-t0JR7KMMtV4`)
@@ -88,14 +34,18 @@ for (let i = 0; i < spots.length; i++) {
     return response.json();
   }).then(jsonData => {
     // stored coordinates and place ids in arrays
-    coord.push(jsonData.results[0].geometry.location);
-    coord.push(jsonData.results[0].geometry.location);
+      coord.push(jsonData.results[0].geometry.location);
+      spotsID.push(jsonData.results[0].place_id)
   })
   .catch(error => {
     console.log(error);
   })
 }
-
+// await call to complete before logging arrays
+setTimeout(function () {
+  console.log(coord);
+  console.log(spotsID);
+}, 100)
 /*
 // Slideshow to randomly display on fade-out-fade-in
 let slideshow = document.querySelector("#slideshow");
