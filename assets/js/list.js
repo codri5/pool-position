fb39f86ac4e88e00a950e16666ef84cc9e2148c2
alicewestcreate <alicewest.create@gmail.com>
2023-02-15 18:41:06 +0000

    js clean up

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,16 +1,18 @@
 const locationsDiv = document.querySelector("#location-cards")
 const locationLink = document.querySelector("#location-link")
-const searchBar = document.querySelector("#search_input")
-const key = "62d5440eb21080da0f55dfad5e6d254d"
-// const key = "fdfc69f0a3fe9836736635c42b79958b" - second key
+// const searchBar = document.querySelector("#search_input")
+const key = "fdfc69f0a3fe9836736635c42b79958b" 
 
 
 
+// Global Variables, for liked button styling. 
 const likeIcon =  "fa-solid fa-heart";
 const unlikeIcon = "fa-regular fa-heart";
 
 
 let updateLocations = function(locations) {
+    // For the first time, locations will equal the locations.js. Once user selects a favourite, 
+    // from then on, locations will equal what is stored in local storage. 
     let locationsFromStorage = localStorage.getItem("locations")
     if (locationsFromStorage === null) {
         return locations
@@ -21,90 +23,58 @@ let updateLocations = function(locations) {
 }
 
 
-// -------------------------- NEED TO KNOW HOW TO RETURN FROM AN AJAX --------------------------------
-
-// let getWeatherIcon = function(lat, lng) {
-//     console.log(lat, lng);
-
-//     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lng + "&appid=" + key + "&units=metric"
-//     let iconURL = ""
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//     }).then(function(response) {
-//         console.log(queryURL)
-//         iconCode = response.list[0].weather[0].icon 
-//         console.log(response);
-//         console.log(iconCode);
-//         iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
-//         console.log("1", iconURL);
-//         return iconURL /// Need to know how to return this item
-
-//     })
-//     console.log("2",iconURL);
-//     return iconURL /// Need to know how to return this item
-
-// }
-
-
-
+// Render the Location card information with data from locations object. 
 let renderLocations = function() {
 
     locationsDiv.innerHTML = '';
 
     locations.forEach(location => {
-   
-        let card = document.createElement("div")
+
+        let card = document.createElement("div")  // Create card container
         card.setAttribute("id", location["name"])
         card.setAttribute("class", "card")
         card.setAttribute("data-fave", location["isFavourite"])
-
-        let imageContainer = document.createElement("div")
+        
+        let imageContainer = document.createElement("div") // Create image container
         imageContainer.setAttribute("class", "image-container")
-
-        let img = document.createElement("img")
+        
+        let img = document.createElement("img") // The photo
         img.setAttribute("class", "card-img-top location-image")
         img.setAttribute("src", location["images"][0])
 
-        let icon = document.createElement("i")
+        let icon = document.createElement("i") // Create icon. Check property to determine which icon
         if (location["isFavourite"] === false) {
             icon.setAttribute("class", unlikeIcon)
         } else if (location["isFavourite"] === true) {
             icon.setAttribute("class", likeIcon)
         }
 
-        let cardBody = document.createElement("div")
+        let cardBody = document.createElement("div") // Create the container for the bottom half of the card
         cardBody.setAttribute("class", "card-body d-flex")
 
-        let textContainer = document.createElement("div")
+        let textContainer = document.createElement("div") // Create the container for the text elements
         textContainer.setAttribute("class", "textContainer col")
 
-
-        let placeName = document.createElement("h5")
+        let placeName = document.createElement("h5") // The Title
         placeName.setAttribute("class", "card-title")
         placeName.innerText = location["name"]
 
-        let locationText = document.createElement("p")
+        let locationText = document.createElement("p") // The Area
         locationText.setAttribute("class", "card-title")
         locationText.innerText = location["area"]
 
-
-        let weatherContainer = document.createElement("div")
+        let weatherContainer = document.createElement("div") // Create the container for the weather elements
         weatherContainer.setAttribute("class", "weatherContainer col d-flex")
 
-        let weatherIcon = document.createElement("img")
+        let weatherIcon = document.createElement("img") // The weather icon. 
         weatherIcon.setAttribute("class", "icon-image row")
-        let lat = location["latlng"]["lat"]
+        let lat = location["latlng"]["lat"] // Getting coords from locations.js
         let lng = location["latlng"]["lng"]
 
         let tempText = document.createElement("p")
         tempText.setAttribute("class", "row")
 
-
-
-
-
+        // Get the current weather for each location. 
         let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lng + "&appid=" + key + "&units=metric"
         let iconURL = ""
         let temp = ""
@@ -113,32 +83,15 @@ let renderLocations = function() {
             url: queryURL,
             method: "GET",
         }).then(function(response) {
-            console.log(queryURL)
             iconCode = response.list[0].weather[0].icon 
             temp = response.list[0].main.temp 
-            console.log(temp);
-            console.log(response);
-            console.log(iconCode);
             iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-            console.log("1", iconURL);
             weatherIcon.setAttribute("src", iconURL)
             tempText.innerText = (`${temp}°C`)
-
         })
-
+    
         
-       
-
-
-
-
-
-        // let weatherIconURL = getWeatherIcon(lat, lng)
-        // console.log(weatherIconURL)
-        // weatherIcon.setAttribute("src", weatherIconURL)
-
-
-
+        // Appending each item to the locationsDiv
         imageContainer.appendChild(icon)
         imageContainer.appendChild(img)
 
@@ -158,7 +111,10 @@ let renderLocations = function() {
     });
 }
 
+
+// If user likes or unlikes, update locations object, and store in local storage. 
 let likeButton = function(event) {
+    // Use an event target to retrieve the card's ID name and to update the property for the corrosponding object.
     let heart = event.target
     let check = heart.tagName
     let parent = heart.parentElement
@@ -166,27 +122,23 @@ let likeButton = function(event) {
     let locationId = grandParent.getAttribute("id")
 
 
+    // As using event deligation, check if the item is an icon > Then change class, update locations object then store in local storage. 
     if (check === "I") {
         if (heart.classList.contains("fa-regular")) {
             heart.setAttribute("class",likeIcon)
             const locationsIndex = locations.findIndex(({ name }) => name === locationId);
-            locations[locationsIndex].isFavourite = true
-       
+            locations[locationsIndex].isFavourite = true   
+        
         } else if (heart.classList.contains("fa-solid")) {
             heart.setAttribute("class",unlikeIcon)
             const locationsIndex = locations.findIndex(({ name }) => name === locationId);
             locations[locationsIndex].isFavourite = false
-    
         }
         let locationsString = JSON.stringify(locations)
         localStorage.setItem("locations", locationsString)
 
     }
 }
-
-
-
-
 
 
 locations = updateLocations(locations)
